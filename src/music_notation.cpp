992b3207af497f4db4a4f4fac299d2df6e93fa52
Daniel Lopez <daniellopez@nevada.unr.edu>
2016-11-25 21:17:30 -0800

    started adding xml string creation

--- src/music_notation.cpp
+++ src/music_notation.cpp
@@ -37,6 +37,51 @@ const char * getPitchString(int enumVal) {
 	return PitchEnumStrings[enumVal];
 }
 
+string getXMLPitchString(int enumVal) {
+	switch (enumVal) {
+		case 1:
+		case 3:
+		case 6:
+		case 8:
+		case 10:
+			return string("<step>") + string(PitchEnumStrings[enumVal-1]) + string("</step>\n<alter>1</alter>");
+		default:
+			return string("<step>") + string(PitchEnumStrings[enumVal]) + string("</step>");
+	}
+}
+
+enum NoteType{
+	WHOLE = 0,
+	HALF,
+	QUARTER,
+	EIGHTH,
+	SIXTEENTH
+};
+
+const char * NoteTypeEnumStrings[] = {
+	"WHOLE",
+	"HALF",
+	"QUARTER",
+	"EIGHTH",
+	"16TH"	
+};
+
+string getNoteTypeString(int enumVal) {
+	return NoteTypeEnumStrings[enumVal];
+}
+
+string getNoteTypeStringForBeatFactor(float beat_factor) {
+	if (beat_factor == 4)
+		return getNoteTypeString(0);
+	else if (beat_factor == 2)
+		return getNoteTypeString(1);
+	else if (beat_factor == 1)
+		return getNoteTypeString(2);
+	else if (beat_factor == 0.5f)
+		return getNoteTypeString(3);
+	else return getNoteTypeString(4);
+}
+
 const Pitch C_FLAT = B;
 const Pitch D_FLAT = C_SHARP;
 const Pitch E_FLAT = D_SHARP;
@@ -101,11 +146,12 @@ public:
 		}
 	}
 
-	void add_notes(const Note* pNotes, unsigned int size) {
+	void add_notes(Note* pNotes, unsigned int size) {
 		Measure measure;
 		float count = 0.0;
 		for (unsigned int i = 0; i < size; i++) {
 			count += pNotes[i].beat_factor;
+			pNotes[i].octave = (Octave)(pNotes[i].octave+3);
 			measure.notes.push_back(pNotes[i]);
 
 			if ((int)count == time_signature.first) {
@@ -137,31 +183,31 @@ public:
 
 	void outputToFile(const string& outFileName) const {
 		ofstream fout;
-		//fout.open(outFileName.c_str());
+		fout.open(outFileName.c_str());
 		
-		cout << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" << endl <<
+		fout << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" << endl <<
 		"<!DOCTYPE score-partwise PUBLIC" << endl << 
 		"   \"-//Recordare//DTD MusicXML 3.0 Partwise//EN\"" << endl <<
 		"   \"http://www.musicxml.org/dtds/partwise.dtd\">" << endl;
 
-    	cout << "<score-partwise version=\"3.0\">" << endl <<
+    	fout << "<score-partwise version=\"3.0\">" << endl <<
   					"<part-list>" << endl <<
 	    				"<score-part id=\"P1\">" << endl <<
 						    "<part-name>" << "MUSIC" << "</part-name>" << endl <<
 						"</score-part>" << endl <<
 					"</part-list>" << endl;
-		cout << "<part id=\"P1\">";
+		fout << "<part id=\"P1\">" << endl;
 
 		for (unsigned int i = 0; i < measures.size(); i++) {
-			cout << "<measure number=\"" << i+1 << "\">" << endl;
+			fout << "<measure number=\"" << i+1 << "\">" << endl;
 			if (i == 0) {
-				cout << "<attributes>" << endl <<
+				fout << "<attributes>" << endl <<
 			        "<divisions>1</divisions>" << endl <<
 			        "<key>" << endl <<
 			          "<fifths>0</fifths>" << endl <<
 			        "</key>" << endl <<
 			        "<time>" << endl <<
-			          "<beats>4</beats>" << endl <<
+			          "<beats>3</beats>" << endl <<
 			          "<beat-type>4</beat-type>" << endl <<
 			        "</time>" << endl <<
 			        "<clef>" << endl <<
@@ -170,22 +216,38 @@ public:
 			        "</clef>" << endl <<
 			      "</attributes>" << endl;
 			}
-		    cout << "<note>" << endl <<
-		        "<pitch>" << endl <<
-		        	"<step>C</step>" << endl <<
-		        	"<octave>4</octave>" << endl <<
-		        "</pitch>" << endl <<
-		        "<duration>4</duration>" << endl <<
-		        "<type>whole</type>" << endl <<
-		    "</note>" << endl <<
-		    "</measure>" << endl;
+
+			for (unsigned int j = 0; j < measures[i].notes.size(); j++) {
+				if (measures[i].notes[j].pitch != REST) {
+				    fout << "<note>" << endl <<
+				        "<pitch>" << endl <<
+				        	getXMLPitchString(measures[i].notes[j].pitch) << endl <<
+				        	"<octave>" << measures[i].notes[j].octave << "</octave>" << endl <<
+				        "</pitch>" << endl <<
+				        "<duration>" << measures[i].notes[j].beat_factor * 2 << "</duration>" << endl <<
+				        "<type>" << getNoteTypeStringForBeatFactor(measures[i].notes[j].beat_factor) << "</type>" << endl <<
+				    "</note>" << endl;
+				} else {
+					fout << "<note>" << endl <<
+				        "<rest/>" << endl << 
+				        "<duration>" << measures[i].notes[j].beat_factor * 2 << "</duration>" << endl <<
+				        "<type>" << getNoteTypeStringForBeatFactor(measures[i].notes[j].beat_factor) << "</type>" << endl <<
+				    "</note>" << endl;
+				}
+			}
+			fout << "</measure>" << endl;
+
 		}
 
-		cout << "</part>" << endl;
-		cout << "</score-partwise>" << endl;
+		fout << "</part>" << endl;
+		fout << "</score-partwise>" << endl;
 
 
 		fout.close();
+	}
+
+	void outputToFileUsingTiny(const string& outFileName) const {
+		
 	}
 
 private:
@@ -244,7 +306,7 @@ int main(int argc, char const *argv[]) {
 	testPassage.add_notes(tune, 30);
 
 	testPassage.print();
-	testPassage.outputToFile("");
+	testPassage.outputToFile("../bin/testoutput.xml");
 
 
 	return 0
